31c7819291dccca7dad5973df6ecfd545e1b8968
isabella <115096194+sevenf0ld@users.noreply.github.com>
2024-04-07 19:17:12 +0800

    add Bureaucrat (ex00)

---
+++ ex01/src/Bureaucrat.cpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 10:09:30 by maiman-m          #+#    #+#             */
+/*   Updated: 2024/04/06 10:02:19 by maiman-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+	: name_("def"), grade_(150)
+{
+	FORMAT_COPY_CONTROL("Default ctor called.");
+}
+
+// catching exceptions in member-initalizer list
+// (source: https://stackoverflow.com/a/160164)
+Bureaucrat::Bureaucrat(std::string name, int grade)
+try : name_(name), grade_(grade)
+{
+	FORMAT_COPY_CONTROL("Parameterized ctor called.");
+	if (grade > 150)
+		throw GradeTooLowException();
+	else if (grade < 1)
+		throw GradeTooHighException();
+}
+catch (const GradeTooHighException &e)
+{
+	COPY_CONTROL_FAILURE("Parameterized ctor failed: ", e.what());
+}
+catch (const GradeTooLowException &e)
+{
+	COPY_CONTROL_FAILURE("Parameterized ctor failed: ", e.what());
+}
+// Bureaucrat::Bureaucrat(std::string name, int grade)
+//		  : name_(name), grade_(grade)
+//{
+//	FORMAT_COPY_CONTROL("Parameterized ctor called.");
+//	if (grade < 1)
+//		throw GradeTooHighException();
+//	else if (grade > 150)
+//		throw GradeTooLowException();
+//	else
+//		grade_ = grade;
+// }
+
+Bureaucrat::Bureaucrat(const Bureaucrat &rhs)
+	: name_(rhs.name_), grade_(rhs.grade_)
+{
+	FORMAT_COPY_CONTROL("Copy ctor called.");
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	if (this != &rhs)
+		grade_ = rhs.grade_;
+	FORMAT_COPY_CONTROL("Assignment operator called.");
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	FORMAT_COPY_CONTROL("Dtor called.");
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high (1 is the highest grade).");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low (150 is the lowest grade).");
+}
+
+const std::string Bureaucrat::getName(void) const
+{
+	return (name_);
+}
+
+int Bureaucrat::getGrade(void) const
+{
+	return (grade_);
+}
+
+Bureaucrat &Bureaucrat::operator++()
+{
+	if (grade_ - 1 < 1)
+		throw GradeTooHighException();
+	else
+	{
+		grade_ -= 1;
+		return (*this);
+	}
+}
+
+Bureaucrat &Bureaucrat::operator--()
+{
+	if (grade_ + 1 > 150)
+		throw GradeTooLowException();
+	else
+	{
+		grade_ += 1;
+		return (*this);
+	}
+}
+
+std::ostream &operator<<(std::ostream &out_stream, const Bureaucrat &rhs)
+{
+	out_stream << AC_CYAN << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << AC_NORMAL;
+	return (out_stream);
+}
---
+++ ex01/src/main.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 11:12:44 by maiman-m          #+#    #+#             */
+/*   Updated: 2024/04/06 10:03:41 by maiman-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	FORMAT_TEST("ORTHODOX CANONICAL FORM TEST");
+	Bureaucrat def;
+	Bureaucrat copy(def);
+	Bureaucrat assign;
+	assign = def;
+	std::cout << std::endl;
+
+	FORMAT_TEST("MAX AND MIN GRADE TEST");
+	Bureaucrat max("max", 1);
+	Bureaucrat min("min", 150);
+	std::cout << "\t" << max << std::endl
+			  << "\t" << min << std::endl;
+	std::cout << std::endl;
+
+	FORMAT_TEST("BEYOND MAX AND MIN GRADE TEST");
+	try
+	{
+		Bureaucrat beyond_max("beyond_max", 0);
+	}
+	catch (const Bureaucrat::GradeTooHighException &e)
+	{
+		;
+	}
+	try
+	{
+		Bureaucrat beyond_min("beyond_min", 151);
+	}
+	catch (const Bureaucrat::GradeTooLowException &e)
+	{
+		;
+	}
+	std::cout << std::endl;
+
+	FORMAT_TEST("DECREMENT MAX GRADE TEST");
+	std::cout << max << " to " << --max << std::endl;
+	std::cout << std::endl;
+
+	FORMAT_TEST("INCREMENT AS PER SUBJECT PDF");
+	Bureaucrat subject("subject", 3);
+	std::cout << subject << " to " << ++subject << std::endl;
+	std::cout << std::endl;
+
+	FORMAT_TEST("DECREMENT BEYOND THE MIN GRADE TEST");
+	try
+	{
+		--min;
+	}
+	catch (const Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << min << std::endl;
+	std::cout << std::endl;
+
+	++max;
+	FORMAT_TEST("INCCREMENT BEYOND THE MAX GRADE TEST");
+	try
+	{
+		++max;
+	}
+	catch (const Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << max << std::endl;
+	std::cout << std::endl;
+}
